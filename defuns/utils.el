7861d5af96afd9febff7dac97e90242369f78fd8
Aleksandre Khutsishvili <a.khutsishvili@emis.ge>
2015-10-21 14:47:33 +0400

    just to ansi-term buffer

--- defuns/utils.el
+++ defuns/utils.el
@@ -27,4 +27,14 @@
   (interactive "p")
   (shift-text (- count)))
 
+(defun visit-term-buffer ()
+  "Create or visit a terminal buffer."
+  (interactive)
+  (if (not (get-buffer "*ansi-term*"))
+      (progn
+        (split-window-sensibly (selected-window))
+        (other-window 1)
+        (ansi-term (getenv "SHELL")))
+    (switch-to-buffer-other-window "*ansi-term*")))
+
 (provide 'utils)
