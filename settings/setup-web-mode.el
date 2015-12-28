c9a97c57238be231f6d4540dde7110b972a3d228
Aleksandre Khutsishvili <a.khutsishvili@emis.ge>
2015-12-28 14:02:40 +0400

    add {{}} insert

--- settings/setup-web-mode.el
+++ settings/setup-web-mode.el
@@ -1,3 +1,10 @@
+;; mode specific key bindings
+(global-set-key (kbd "C-\{") (lambda ()
+                               (interactive)
+                               (insert "{{}}")
+                               (backward-char)
+                               (backward-char)))
+
 (custom-set-variables
 	'(web-mode-enable-auto-expanding t)
  	'(web-mode-enable-current-element-highlight t)
@@ -6,6 +13,3 @@
     '(web-mode-code-indent-offset 4)
     '(web-mode-indent-style 4)
  )
-
-
-
