42fcc7ee89b7a057a7b08b988a57f03323cf267d
Aleksandre Khutsishvili <a.khutsishvili@emis.ge>
2016-06-03 15:05:29 +0400

    add hooks

--- settings/setup-js2-mode.el
+++ settings/setup-js2-mode.el
@@ -1,7 +1,10 @@
 (autoload 'js2-mode "js2-mode" nil t)
+(require 'js3-refactor)
+(js3r-add-keybindings-with-prefix "C-c C-m")
 
 (add-hook 'js2-mode-hook
 	  (lambda ()
-	      (auto-complete-mode t)))
+	      (auto-complete-mode t)
+(add-hook 'js2-mode-hook #'js2-refactor-mode)))
 
 (provide 'setup-js2-mode)
